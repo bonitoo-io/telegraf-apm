1dda4a882cec10b5032ad3f14c73c9770071d9db
Robert Hajek <robert.hajek@gmail.com>
2020-04-27 18:24:28 +0200

    - updated rails demo to use postgres, added simple blog post demo

--- demo-rails-app/config/routes.rb
+++ demo-rails-app/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :posts
+
   get 'error/error'
   get 'welcome/index'
+  root :to => 'posts#index'
 
-  root 'welcome#index'
+  # root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- run-telegraf-apm-demo.sh
+++ run-telegraf-apm-demo.sh
@@ -7,6 +7,8 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 docker rm -f demo-rails-app || true
 docker rm -f demo-java-app || true
 docker rm -f telegraf-apm || true
+docker rm -f postgres-demo || true
+docker rm -f redis-demo || true
 
 docker network rm apm_network || true
 docker network create -d bridge apm_network --subnet 192.168.1.0/24 --gateway 192.168.1.1 || true
@@ -20,10 +22,23 @@ docker run -d --name telegraf-apm \
   --volume="$(pwd)/telegraf/telegraf.conf:/etc/telegraf/telegraf.conf:ro" \
   telegraf-apm
 
+docker run -d --name postgres-demo \
+  --network apm_network \
+  -e POSTGRES_PASSWORD=postgres \
+  -p 5432:5432 \
+  postgres
+
+docker run -d --name redis-demo \
+  --network apm_network \
+  -p 6379:6379 \
+  redis
+
+#### attach redis cli client
+#docker run -it --name my-redis-cli --network apm_network --rm redis redis-cli -h redis-demo -p 6379
+
 ## build rails demo app image
 docker -v  build ./demo-rails-app -t demo-rails-app
 docker -v  build ./demo-java-app -t demo-java-app
-
 
 docker run -d --name demo-java-app \
   --network apm_network \
@@ -37,4 +52,8 @@ docker run -d --name=demo-rails-app \
   -p 3000:3000 \
   --env ELASTIC_APM_SERVER_URL=http://telegraf-apm:8200 \
   --env ELASTIC_APM_TRANSACTION_SAMPLE_RATE=0.5 \
+  --env POSTGRES_HOST=postgres-demo \
   demo-rails-app:latest
+
+## initialize database
+docker exec -it demo-rails-app rails db:migrate:reset
